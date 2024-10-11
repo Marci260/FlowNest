e6ebdaf1046b39eb0d1363b56511339bc4fdb4b8
Marci260 <marcikanya@gmail.com>
2024-10-11 18:08:10 +0200

    using ReleaseDate instead of Year

--- FlowNest/Models/Movie.cs
+++ FlowNest/Models/Movie.cs
@@ -6,16 +6,16 @@
         //class for mive entity, with id, title,year, genre,director, rating
         public string Id { get; set; }
         public string Title { get; set; }
-        public int Year { get; set; }
+        public int ReleasedDate { get; set; }
         public string Genre { get; set; }
         public string Director { get; set; }
         public int Rating { get; set; }
         // constructor
-        public Movie( string title, int year, string genre, string director, int rating)
+        public Movie( string title, int releasedDate, string genre, string director, int rating)
         {
             Id = System.Guid.NewGuid().ToString();
             Title = title;
-            Year = year;
+            ReleasedDate = releasedDate;
             Genre = genre;
             Director = director;
             Rating = rating;
