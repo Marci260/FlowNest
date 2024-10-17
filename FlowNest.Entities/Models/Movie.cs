80af9212cbbe7df3ec4c6ea0151a26723c7260c7
Marci260 <marcikanya@gmail.com>
2024-10-17 14:29:48 +0200

    decorators for movie class

--- FlowNest.Entities/Models/Movie.cs
+++ FlowNest.Entities/Models/Movie.cs
@@ -1,4 +1,7 @@
-﻿namespace FlowNest.Entities.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlowNest.Entities.Models
 {
 
     public enum Genre
@@ -20,21 +23,25 @@
     public class Movie
     {
         //class for mive entity, with id, title,year, genre,director, rating
+        [StringLength(50)]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
+        [StringLength(100)]
         public string Title { get; set; }
         public int ReleasedDate { get; set; }
         public Genre Genre { get; set; }
         public string Director { get; set; }
-        public int Rating { get; set; }
+        [NotMapped]
+        public virtual ICollection<Rating>? Ratings { get; set; }
         // constructor
-        public Movie(string title, int releasedDate, Genre genre, string director, int rating)
+        public Movie(string title, int releasedDate, Genre genre, string director)
         {
             Id = System.Guid.NewGuid().ToString();
             Title = title;
             ReleasedDate = releasedDate;
             Genre = genre;
             Director = director;
-            Rating = rating;
         }
         //empty constructor
         public Movie()
